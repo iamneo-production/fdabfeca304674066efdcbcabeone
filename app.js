ce4382a80822b1525e789a0adc03ff148bfb93e4
Iamneo <techadmin@iamneo.ai>
2023-09-03 10:22:32 +0000

    Backup Commit

--- app.js
+++ app.js
@@ -2,4 +2,23 @@ let cells=[",",",",",",",","];
 let currentPlayer='X';
 let result=document.querySelector('.result');
 let btns=document.querySelectorAll('.btn');
-let conditions=
+let conditions=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+const ticTacToe=(element,index)=>{
+    if(cells[index]!=="|| result.textContent!=="){
+        return;
+    }
+    cells[index]=currentPlayer;
+    element.textContent=currentPlayer;
+    }
+    for(const condition of conditions){
+        const[a,b,c]=
+    }
